2afbf32ed6993055ddbcb889ff0467ddcaa8bb62
Aziza Helmy <aziza.helmy35@gmail.com>
2022-10-04 23:38:59 +0200

    Initial commit with all dependencies

---
+++ app/src/main/java/com/aziza/photosapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aziza.photosapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
